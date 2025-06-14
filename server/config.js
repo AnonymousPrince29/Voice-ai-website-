f03aca7cc082f8aebd957b448565bed3c5619b52
AnonymousPrince29 <67414076+AnonymousPrince29@users.noreply.github.com>
2025-06-14 10:19:55 +0300

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     // JWT configuration
     jwt: {
-        secret: process.env.JWT_SECRET || 'sk_6d07ebb8c2262774b366e3b0e2caf4993c0e5ab898d7f138',
+        secret: process.env.JWT_SECRET || 'your-secret-key',
         expiresIn: '30d'
     },
     
